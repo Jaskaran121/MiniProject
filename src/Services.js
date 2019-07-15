97536fe6e8a6b5855e719ce282827ee0f6be7ffb
Jaskaran Kukreja <35613378+Jaskaran121@users.noreply.github.com>
2019-07-15 15:36:09 -0400

    Update Services.js

--- src/Services.js
+++ src/Services.js
@@ -1,7 +1,17 @@
 import axios from "axios";
 
 export async function getEvents() {
-  var events = [];
   const getEvents = await axios.get('https://gitlab.com/snippets/1871557/raw');
   return getEvents.data;
+}
+
+export async function postEvents(data){
+  console.log(data)
+  //const getEvents = await axios.post("Put your localhost api url here",{
+  //   event:data.currentEvent,
+  //   email:data.email,
+  //   name:data.name,
+  //   city:data.currentCity
+  // })
+  return data
 }
